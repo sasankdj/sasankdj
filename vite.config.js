af801e5f9fbb359ada4d1b2a4b0b6e656b4f9819
sasank <kotasasank123@gmail.com>
2025-07-25 23:01:30 +0530

    1st commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Sasank-portfolio",
+  base: "/sasankdj",
   plugins: [tailwindcss(),react()],
 })
